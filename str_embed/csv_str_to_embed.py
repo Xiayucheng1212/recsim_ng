4da1073670a5c45ad893670b24757a403a6d5b99
jerryshu98 <jerryshu98@gmail.com>
2023-10-18 07:44:15 +0800

    update group by encode

--- str_embed/csv_str_to_embed.py
+++ str_embed/csv_str_to_embed.py
@@ -1,4 +1,3 @@
-# imports
 import pandas as pd
 import tiktoken
 import openai
@@ -6,22 +5,26 @@ from openai.embeddings_utils import get_embedding
 from sklearn.preprocessing import LabelEncoder
 label_encoder = LabelEncoder()
 
-openai.api_key = "your-openai-key"
+openai.api_key = ""
 embedding_model = "text-embedding-ada-002"
-embedding_encoding = "cl100k_base"  
-max_tokens = 8000  
-top_n = 20
-input_datapath = "./data/csv_rawdata.csv"  
-output_datapath = "./data/embeddings.csv"
+embedding_encoding = "cl100k_base"
+input_datapath = "./data/csv_rawdata.csv"
+output_datapath = "./data/embeddings_10k.csv"
 
 df = pd.read_csv(input_datapath)
 df['category_encoded'] = label_encoder.fit_transform(df['category'])
-df = df.head(top_n)
 df["combined"] = (
     "Title: " + df.headline.str.strip() + "; Content: " + df.short_description.str.strip()
 )
-print(df["combined"])
-df["embedding"] = df.combined.apply(lambda x: get_embedding(x, engine=embedding_model))
+#create group by encode number
+result_dfs = []
+for encode, group in df.groupby('category_encoded'):
+    sampled_group = group.head(300).dropna()
+    sampled_group = sampled_group.head(240)
+    result_dfs.append(sampled_group)
+df = pd.concat(result_dfs)
 
+#create embedding
+df["embedding"] = df.combined.apply(lambda x: get_embedding(x, engine=embedding_model))
 df = df.drop(["date","link","authors","category","headline","short_description","combined"], axis='columns')
 df.to_csv(output_datapath)
