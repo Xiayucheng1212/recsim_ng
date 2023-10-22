958abd83a678ab8b6183b0a1577895c64b436cba
jerryshu98 <jerryshu98@gmail.com>
2023-10-22 21:02:39 +0800

    add gen1 embed model

--- str_embed/csv_str_to_embed.py
+++ str_embed/csv_str_to_embed.py
@@ -4,17 +4,18 @@ import openai
 from openai.embeddings_utils import get_embedding
 from sklearn.preprocessing import LabelEncoder
 label_encoder = LabelEncoder()
-
-openai.api_key = ""
-embedding_model = "text-embedding-ada-002"
+print("?")
+openai.api_key = "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"
+embedding_model1 = "text-embedding-ada-002"
+embedding_model2 = "text-similarity-babbage-001"
 embedding_encoding = "cl100k_base"
 input_datapath = "./data/csv_rawdata.csv"
-output_datapath = "./data/embeddings_10k.csv"
+output_datapath = "./data/embeddings.csv"
 
 df = pd.read_csv(input_datapath)
 df['category_encoded'] = label_encoder.fit_transform(df['category'])
 df["combined"] = (
-    "Title: " + df.headline.str.strip() + "; Content: " + df.short_description.str.strip()
+    "Title: " + df.category.str.strip() + "; Content: " + df.headline.str.strip()
 )
 #create group by encode number
 number_each_encode = 240
@@ -26,6 +27,7 @@ for encode, group in df.groupby('category_encoded'):
 df = pd.concat(result_dfs)
 
 #create embedding
-df["embedding"] = df.combined.apply(lambda x: get_embedding(x, engine=embedding_model))
+df["embedding_cat_headline"] = df.combined.apply(lambda x: get_embedding(x, engine=embedding_model1))
+df["embedding_description"] = df.short_description.apply(lambda x: get_embedding(x, engine=embedding_model2))
 df = df.drop(["date","link","authors","category","headline","short_description","combined"], axis='columns')
 df.to_csv(output_datapath)
