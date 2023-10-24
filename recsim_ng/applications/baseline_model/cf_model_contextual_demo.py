6d67d57a85254f4e8992c9e3b7f0cf70b4d53d0a
CHIUWEINING <william08162010@gmail.com>
2023-10-24 21:07:59 +0800

    revise cf_model_contextual_demo

--- recsim_ng/applications/baseline_model/cf_model_contextual_demo.py
+++ recsim_ng/applications/baseline_model/cf_model_contextual_demo.py
@@ -3,6 +3,7 @@ import time
 
 from absl import app
 from recsim_ng.applications.baseline_model import simulation_config
+from recsim_ng.applications.baseline_model import cf_interest_evolution_simulation
 from recsim_ng.core import network as network_lib
 from recsim_ng.lib.tensorflow import runtime
 import tensorflow as tf
@@ -52,13 +53,38 @@ def run_simulation(num_runs, num_users, horizon, history_length):
 def main(argv):
     del argv
     num_runs = 3
+    num_training_steps = 20
     num_users = 1
-    horizon = 1000
+    horizon = 50
     history_length = 15
     t_begin = time.time()
-    reward_mean, avg_ctr = run_simulation(num_runs, num_users, horizon, history_length)
+    # -----------------------------------------
+    sum_user_creward = 0.0
+    sum_ctr = 0.0
+    for _ in range(num_runs):
+        variables, trainable_variables = (
+        simulation_config.create_cf_simulation_network(
+            num_users=num_users, history_length=history_length))
+        
+        results = cf_interest_evolution_simulation.run_simulation(
+        num_training_steps=num_training_steps,
+        horizon=horizon,
+        global_batch=num_users,
+        learning_rate=1e-4,
+        simulation_variables=variables,
+        trainable_variables=trainable_variables,
+        metric_to_optimize='cumulative_reward')
+
+        sum_ctr += results[1]
+        sum_user_creward += results[0]
+    
+    creward_mean = sum_user_creward / num_runs
+    avg_ctr = sum_ctr / num_runs
+
+    # -----------------------------------------
+    # reward_mean, avg_ctr = run_simulation(num_runs, num_users, horizon, history_length)
     print('Elapsed time: %.3f seconds' %(time.time() - t_begin))
-    print('Average reward: %f' %reward_mean)
+    print('Average reward: %f' %creward_mean)
     print('Average ctr: %f' %avg_ctr)
     # 0.47 -> new doc_vector but epsilon 1.0 -> 0.9
     # 0.45 -> new doc_vector but epsilon 0.5 -> 0.45
--- recsim_ng/applications/baseline_model/simulation_config.py
+++ recsim_ng/applications/baseline_model/simulation_config.py
@@ -1,4 +1,6 @@
 """Configuration parameters for running ecosystem simulation."""
+import functools
+import tensorflow as tf
 from typing import Collection
 import gin
 import numpy as np
@@ -8,6 +10,7 @@ from recsim_ng.applications.baseline_model import recommender
 from recsim_ng.applications.baseline_model import user_interest
 from recsim_ng.core import variable
 from recsim_ng.stories import recommendation_simulation as simulation
+from recsim_ng.lib.tensorflow import entity
 
 Variable = variable.Variable
 
@@ -20,30 +23,34 @@ def create_cf_simulation_network(
     freeze_user = True,
     history_length = 15
 ):
-    num_docs = 9750 #9750
-    config = {
+  tf.config.run_functions_eagerly(True)
+  """Returns a network for interests evolution simulation."""
+  num_docs = 9750 #9750
+  config = {
         'num_topics': num_topics,
         'doc_embed_dim': doc_embed_dim,
         'num_users': num_users,
         'num_docs': num_docs,
         'slate_size': slate_size,
         'data_path': './str_embed/data',
+        # History length for user representation in recommender.
         'history_length': history_length
-    }
-
-    if freeze_user:
+  }
+  if freeze_user:
         user_init = lambda config: user_interest.InterestEvolutionUser(  # pylint: disable=g-long-lambda
             config).initial_state()
         user_ctor = lambda config: user_interest.StaticUser(config, user_init(config))
-    else:
+  else:
         user_ctor = user_interest.InterestEvolutionUser
+  var_fn = lambda: simulation.recs_story(  # pylint: disable=g-long-lambda
+      config, user_ctor, corpus.CorpusWithEmbeddingsAndTopics,
+      functools.partial(recommender.CollabFilteringRecommender),\
+      metrics.SuccessRateMetrics,
+      metrics.ClickThroughRateAsRewardMetrics)
+  simulation_vars, trainable_vars = entity.story_with_trainable_variables(
+      var_fn)
+  return simulation_vars, trainable_vars['Recommender']
 
-    return simulation.recs_story(config, 
-                    user_ctor,
-                    corpus.CorpusWithEmbeddingsAndTopics,
-                    recommender.CollabFilteringRecommender,
-                    metrics.SuccessRateMetrics,
-                    metrics.ClickThroughRateAsRewardMetrics)
 
 @gin.configurable
 def create_random_simulation_network(
